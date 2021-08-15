6a88958697b1cd54809735ede71cb3adaf8006a8
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-15 14:48:41 -0500

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Jessica Perez on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
