58638e488b218d3365993b47321bcfc44167a4e1
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-20 02:03:51 -0500

    - created my character cell with a rounded rectangle shadow drop look

--- dif aproach/CharactherCell.swift
+++ dif aproach/CharactherCell.swift
@@ -6,15 +6,54 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
+
+
 
 struct CharactherCell: View {
+   // var char: Characthers
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack { //background layer of cell
+            VStack(alignment: .leading, spacing: 0) {
+                Image("icon")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(5)
+                    .clipped()
+                    .frame(width: 200, height: 200)
+                
+                VStack(alignment: .center, spacing: 0  ) {
+                   Text("Walter White")
+                    .frame(width: 200, height: 40, alignment: .center)
+                    
+                    HStack{
+                            Text("Heinsenburg")
+                                .font(.headline)
+                                .bold()
+                                .foregroundColor(.black)
+                                .frame(width: 200, height: 40, alignment: .center)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 20).fill(Color.brandPrimary.opacity(0.50))
+                                    )
+                                .frame(width: 100, height: 100)
+                    }
+                    
+                }
+            }
+            
+        }//:end zstack
+        .background(Color.white)
+        .cornerRadius(12)
+        .shadow(color: .brandPrimary,radius: 6, x:0.0, y:0.0)
+        
     }
 }
 
 struct CharactherCell_Previews: PreviewProvider {
     static var previews: some View {
         CharactherCell()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
