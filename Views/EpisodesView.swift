5d293b922e86297c9386f8ecace75e78c31199f0
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-22 12:43:46 -0500

    - fixed coporrupt files

---
+++ Views/EpisodesView.swift
@@ -0,0 +1,48 @@
+//
+//  EpisodesView.swift
+//  BreakingBad
+//
+//  Created by Jessica Perez on 8/22/21.
+//
+
+
+import SwiftUI
+//MARK: -Properties
+struct EpisodesView: View {
+        
+        @StateObject var episodesViewModel = EpisodesViewModel() //observable obj
+        
+    
+        //MARK: -Body
+        var body: some View {
+            NavigationView{
+                List {
+                    ForEach(episodesViewModel.groupedEpisodes) { season in
+                        Section(header: Text(season.title)) {
+                            ForEach(season.episodes) { episode in
+                                Text(episode.title)
+                                    .foregroundColor(.brandPrimary)
+                            }
+                        }
+                    }
+                
+                }
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("Episodes")
+                .onAppear {
+            episodesViewModel.fetchEpisodes()
+                }
+            }
+
+               
+                    }
+                }
+          // }
+
+    //MARK: Preview
+    struct EpisodesView_Previews: PreviewProvider {
+        static var previews: some View {
+            EpisodesView()
+        }
+    }
+
