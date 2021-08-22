09dd75e2bd630bf9dc684185bed345b5d0102622
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-22 02:51:19 -0500

    - final changes

--- Views/EpisodesView.swift
+++ Views/EpisodesView.swift
@@ -8,38 +8,51 @@
 import SwiftUI
 //MARK: -Properties
 struct EpisodesView: View {
+        
+        @ObservedObject var episodesViewModel = EpisodesViewModel() //observable obj
+        
+        
+        
     
-    @ObservedObject var episodesViewModel = EpisodesViewModel() //observable obj
-    
-    //MARK: -Body
-    
-    private var selectedEpisode: Episodes?
-    
-    var body: some View {
-        NavigationView{
-                List(episodesViewModel.episode){ episode  in
-                    NavigationLink(
-                        destination: Text("Destination"),
-                        label: {
-                            Text("Season\(episode.season)")
-                        })
-                    Text(episode.episode)
-                    
-                }//subview of nav
-                .listStyle(GroupedListStyle())
+        //MARK: -Body
+        var body: some View {
+            NavigationView{
+                ZStack{
+                    List {
+                        ForEach(episodesViewModel.episode) { episode in
+                            VStack(alignment: .leading) {
+                                Text("Season\(episode.season)")
+                                    .font(.title2)
+                                Text("Episode\(episode.episode)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.brandPrimary)
+                                    
+                                Spacer() 
+                                HStack {
+                                    Text(episode.title).lineLimit(1)
+                                        .font(.headline)
+                               
+                                }
+                                
+                            }
+                        }
+                    }
+                }
                 .navigationTitle("Episodes")
-                .foregroundColor(.brandPrimary)
                 .onAppear {
-                    episodesViewModel.fetchEpisodes()
+            episodesViewModel.fetchEpisodes()
                 }
             }
+
+               
+                    }
+                }
+          // }
+
+    //MARK: Preview
+    struct EpisodesView_Previews: PreviewProvider {
+        static var previews: some View {
+            EpisodesView()
         }
     }
-      
 
-//MARK: Preview
-struct EpisodesView_Previews: PreviewProvider {
-    static var previews: some View {
-        EpisodesView()
-    }
-}
